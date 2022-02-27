3d09b0974cf588a949647f939c769bdeadaa7777
Alex Munene <62714471+enenumxela@users.noreply.github.com>
2022-02-27 10:54:59 +0300

    chore: -

--- client.go
+++ client.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"os"
-	"strings"
 	"time"
 
 	"github.com/enenumxela/sshx/authentication"
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 // Client is the structure of sshx client.
@@ -22,11 +19,11 @@ type Client struct {
 
 // Configuration is the structure of configuration for sshx client.
 type Configuration struct {
-	Auth            authentication.Authentication
-	User            string
 	Addr            string
 	Port            uint
+	User            string
 	Timeout         int
+	Auth            authentication.Authentication
 	HostKeyCallback ssh.HostKeyCallback
 }
 
@@ -45,137 +42,20 @@ func New(configuration *Configuration) (client *Client, err error) {
 		SFTP: &sftp.Client{},
 	}
 
-	if configuration.Timeout == 0 {
-		configuration.Timeout = 20
-	}
+	// if configuration.Timeout == 0 {
+	// 	configuration.Timeout = 20
+	// }
 
-	client.SSH, err = ssh.Dial("tcp", net.JoinHostPort(configuration.Addr, fmt.Sprint(configuration.Port)), &ssh.ClientConfig{
+	if client.SSH, err = ssh.Dial("tcp", net.JoinHostPort(configuration.Addr, fmt.Sprint(configuration.Port)), &ssh.ClientConfig{
 		User:            configuration.User,
 		Auth:            configuration.Auth,
 		Timeout:         time.Duration(configuration.Timeout) * time.Second,
 		HostKeyCallback: configuration.HostKeyCallback,
-	})
-	if err != nil {
+	}); err != nil {
 		return
 	}
 
-	client.SFTP, err = sftp.NewClient(client.SSH)
-	if err != nil {
-		return
-	}
-
-	return
-}
-
-// GetShell
-func (client *Client) GetShell() (err error) {
-	session, err := client.SSH.NewSession()
-	if err != nil {
-		return
-	}
-
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          1,     // enable echoing
-		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-	}
-
-	term := os.Getenv("TERM")
-
-	if term == "" {
-		term = "xterm-256color"
-	}
-
-	fd := int(os.Stdin.Fd())
-
-	state, err := terminal.MakeRaw(fd)
-	if err != nil {
-		return
-	}
-
-	defer terminal.Restore(fd, state)
-
-	w, h, err := terminal.GetSize(fd)
-	if err != nil {
-		return
-	}
-
-	if err = session.RequestPty(term, h, w, modes); err != nil {
-		return
-	}
-
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-	session.Stdin = os.Stdin
-
-	if err = session.Shell(); err != nil {
-		return
-	}
-
-	if err = session.Wait(); err != nil {
-		if e, ok := err.(*ssh.ExitError); ok {
-			switch e.ExitStatus() {
-			case 130:
-				return nil
-			}
-		}
-
-		return
-	}
-
-	return
-}
-
-// RunCommand
-func (client *Client) RunCommand(command *Command) (err error) {
-	session, err := client.SSH.NewSession()
-	if err != nil {
-		return
-	}
-
-	defer session.Close()
-
-	for _, env := range command.Env {
-		variable := strings.Split(env, "=")
-		if len(variable) != 2 {
-			continue
-		}
-
-		if err := session.Setenv(variable[0], variable[1]); err != nil {
-			return err
-		}
-	}
-
-	if command.Stdin != nil {
-		stdin, err := session.StdinPipe()
-		if err != nil {
-			return err
-		}
-
-		go io.Copy(stdin, command.Stdin)
-	}
-
-	if command.Stdout != nil {
-		stdout, err := session.StdoutPipe()
-		if err != nil {
-			return err
-		}
-
-		go io.Copy(command.Stdout, stdout)
-	}
-
-	if command.Stderr != nil {
-		stderr, err := session.StderrPipe()
-		if err != nil {
-			return err
-		}
-
-		go io.Copy(command.Stderr, stderr)
-	}
-
-	command.CMD = "source ~/.profile && " + command.CMD
-
-	if err = session.Run(command.CMD); err != nil {
+	if client.SFTP, err = sftp.NewClient(client.SSH); err != nil {
 		return
 	}
 
---
+++ ssh.go
@@ -0,0 +1,129 @@
+package sshx
+
+import (
+	"io"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+// GetShell
+func (client *Client) GetShell() (err error) {
+	session, err := client.SSH.NewSession()
+	if err != nil {
+		return
+	}
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          1,     // enable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	term := os.Getenv("TERM")
+
+	if term == "" {
+		term = "xterm-256color"
+	}
+
+	fd := int(os.Stdin.Fd())
+
+	state, err := terminal.MakeRaw(fd)
+	if err != nil {
+		return
+	}
+
+	defer terminal.Restore(fd, state)
+
+	w, h, err := terminal.GetSize(fd)
+	if err != nil {
+		return
+	}
+
+	if err = session.RequestPty(term, h, w, modes); err != nil {
+		return
+	}
+
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	session.Stdin = os.Stdin
+
+	if err = session.Shell(); err != nil {
+		return
+	}
+
+	if err = session.Wait(); err != nil {
+		if e, ok := err.(*ssh.ExitError); ok {
+			switch e.ExitStatus() {
+			case 130:
+				return nil
+			}
+		}
+
+		return
+	}
+
+	return
+}
+
+// RunCommand
+func (client *Client) RunCommand(command *Command) (err error) {
+	session, err := client.SSH.NewSession()
+	if err != nil {
+		return
+	}
+
+	// defer session.Close()
+
+	for _, env := range command.Env {
+		variable := strings.Split(env, "=")
+		if len(variable) != 2 {
+			continue
+		}
+
+		if err := session.Setenv(variable[0], variable[1]); err != nil {
+			return err
+		}
+	}
+
+	if command.Stdin != nil {
+		stdin, err := session.StdinPipe()
+		if err != nil {
+			return err
+		}
+
+		go io.Copy(stdin, command.Stdin)
+	}
+
+	if command.Stdout != nil {
+		stdout, err := session.StdoutPipe()
+		if err != nil {
+			return err
+		}
+
+		go io.Copy(command.Stdout, stdout)
+	}
+
+	if command.Stderr != nil {
+		stderr, err := session.StderrPipe()
+		if err != nil {
+			return err
+		}
+
+		go io.Copy(command.Stderr, stderr)
+	}
+
+	command.CMD = "source ~/.profile && " + command.CMD
+
+	if err = session.Run(command.CMD); err != nil {
+		session.Close()
+
+		return
+	}
+
+	session.Close()
+
+	return
+}
